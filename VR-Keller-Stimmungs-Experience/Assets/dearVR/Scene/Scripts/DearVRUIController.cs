4e0a11b15e871609d9bf890fa0085902e22dd486
BlackMagicVR <65305461+BlackMagicVR@users.noreply.github.com>
2020-05-26 01:58:38 +0200

    dearvr
    dearvr

---
+++ VR-Keller-Stimmungs-Experience/Assets/dearVR/Scene/Scripts/DearVRUIController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DearVRUIController : MonoBehaviour {
+
+#pragma warning disable 649
+
+	[SerializeField] GameObject internalReverbObject;
+	[SerializeField] GameObject reverbSendsObject;
+	[SerializeField] GameObject performanceModeObject;
+
+	[SerializeField] CanvasGroup canvasGroupPlayStop;
+	[SerializeField] CanvasGroup canvasGroupPlay;
+	[SerializeField] CanvasGroup canvaspresetChange;
+
+#pragma warning restore 649
+
+	public void SetInternalReverbObject(bool shouldBeActive) {
+		internalReverbObject.SetActive (shouldBeActive);
+		canvasGroupPlayStop.interactable = false;
+		canvasGroupPlay.interactable = false;
+		canvaspresetChange.interactable = true;
+	}
+	public void SetReverbSendsObject(bool shouldBeActive) {
+		reverbSendsObject.SetActive (shouldBeActive);
+		canvasGroupPlayStop.interactable = false;
+		canvasGroupPlay.interactable = false;
+		canvaspresetChange.interactable = false;
+	}	
+	public void SetPerformanceModeObject(bool shouldBeActive) {
+		performanceModeObject.SetActive (shouldBeActive);
+		canvasGroupPlayStop.interactable = true;
+		canvasGroupPlay.interactable = true;
+		canvaspresetChange.interactable = false;
+	}
+}
