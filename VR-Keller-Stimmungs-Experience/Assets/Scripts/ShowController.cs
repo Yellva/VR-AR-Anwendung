66fd2eefe6e9489131e67dfe018d2ba4eeb68299
BlackMagicVR <65305461+BlackMagicVR@users.noreply.github.com>
2020-05-29 19:14:40 +0200

    update 1
    update 1

---
+++ VR-Keller-Stimmungs-Experience/Assets/Scripts/ShowController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Media;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ShowController : MonoBehaviour
+{
+    public bool sc = false;
+
+    void Update()
+    {
+        foreach (var hand in Player.instance.hands)
+        {
+            if (sc)
+            {
+                hand.ShowController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController);
+            }
+            else
+            {
+                hand.HideController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController);
+            }
+        }
+    }
+}
